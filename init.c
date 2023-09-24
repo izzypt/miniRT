b7a17554d2bd26957de1e4de7539b9110786ded9
Simao <bonvalots@gmail.com>
2023-09-24 21:56:44 +0100

    new init settings

--- init.c
+++ init.c
@@ -6,7 +6,7 @@
 /*   By: simao <simao@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/19 22:09:38 by simao             #+#    #+#             */
-/*   Updated: 2023/09/20 22:01:27 by simao            ###   ########.fr       */
+/*   Updated: 2023/09/24 00:30:44 by simao            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,28 +17,70 @@ void	init_values(void)
 {
 	int	i = 0;
 
-	canvas()->width = 1920;
-	canvas()->height = 1080;
+	canvas()->width = 960;
+	canvas()->height = 540;
 	viewport()->width = 1;
 	viewport()->height = 1;
 	viewport()->dist = 1;
 	viewport()->aspect_ratio = (float)16 / (float)9;
 	camera()->x = 0;
 	camera()->y = 0;
-	camera()->z = -5;
-	scene()->spheres = malloc(sizeof(t_Sphere) * 3);
+	camera()->z = 0;
+	scene()->spheres = malloc(sizeof(t_Sphere) * 4);
+	scene()->lights = malloc(sizeof(t_Light) * 3);
+	scene()->spheres[3].radius = 5000;
+	scene()->spheres[3].center.x = 0;
+	scene()->spheres[3].center.y = -5001;
+	scene()->spheres[3].center.z = 3;
+	scene()->spheres[3].color = hex_to_rgb(YELLOW);
 	while (i < 3)
 	{
-		scene()->spheres[i].radius = 0.5 + i / 2;
-		scene()->spheres[i].center.x = 0.5 + i * 2;
-		scene()->spheres[i].center.y = 2 * i;
-		scene()->spheres[i].center.z = 2 * i;
+
 		if (i == 1)
-			scene()->spheres[i].color = RED;
+		{
+			scene()->spheres[i].radius = 1;
+			scene()->spheres[i].center.x = 0;
+			scene()->spheres[i].center.y = -1;
+			scene()->spheres[i].center.z = 3;
+			scene()->spheres[i].color = hex_to_rgb(RED);
+		}
 		else if (i == 0)
-			scene()->spheres[i].color = GREEN;
+		{
+			scene()->spheres[i].radius = 1;
+			scene()->spheres[i].center.x = 2;
+			scene()->spheres[i].center.y = 0;
+			scene()->spheres[i].center.z = 4;
+			scene()->spheres[i].color = hex_to_rgb(BLUE);
+		}
 		else
-			scene()->spheres[i].color = BLUE;
+		{
+			scene()->spheres[i].radius = 1;
+			scene()->spheres[i].center.x = -2;
+			scene()->spheres[i].center.y = 0;
+			scene()->spheres[i].center.z = 4;
+			scene()->spheres[i].color = hex_to_rgb(GREEN);
+		}
+		if (i == 0)
+		{
+			scene()->lights[i].type = 'A';
+			scene()->lights[i].intensity = 0.3;
+		}
+		if (i == 1)
+		{
+			scene()->lights[i].type = 'P';
+			scene()->lights[i].intensity = 0.6;
+			scene()->lights[i].position.x = 2;
+			scene()->lights[i].position.y = 1;
+			scene()->lights[i].position.z = 0;
+		}
+		if (i == 2)
+		{
+			scene()->lights[i].type = 'D';
+			scene()->lights[i].intensity = 0.2;
+			scene()->lights[i].direction.x = 1;
+			scene()->lights[i].direction.y = 4;
+			scene()->lights[i].direction.z = 4;
+		}
 		i++;
 	}
 }
