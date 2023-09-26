288e1a48796405f22db118b11a68a8ccfe70a90a
Simao <bonvalots@gmail.com>
2023-09-26 02:32:31 +0100

    fixed intersection bug due to parentheses placement in discriminant calculation

--- intersections.c
+++ intersections.c
@@ -6,7 +6,7 @@
 /*   By: simao <simao@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 22:33:12 by simao             #+#    #+#             */
-/*   Updated: 2023/09/25 11:53:42 by simao            ###   ########.fr       */
+/*   Updated: 2023/09/26 02:31:46 by simao            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,10 +28,14 @@ float	calculate_light(t_Vector *P, t_Vector *N)
 		else
 		{
 			if (scene()->lights[j].type == 'P')
+			{
 				L = vector_sub(&scene()->lights[j].position, P);
+				//vector_normalize(&L);
+			}
 			if (scene()->lights[j].type == 'D')
 			{
-				L = vector_normalize(&scene()->lights[j].direction);
+				L = scene()->lights[j].direction;
+				//vector_normalize(&L);
 			}
 		}
 		n_dot_l = dot_product(*N, L);
@@ -42,7 +46,7 @@ float	calculate_light(t_Vector *P, t_Vector *N)
 		}
 		j++;
 	}
-	printf("Intensity %f\n", i);
+	//printf("Intensity %f\n", i);
 	return (i);
 }
 
@@ -83,8 +87,6 @@ t_Color	trace_ray(t_Vector ray, int t_min, int t_max)
 	D = vector_mult(&ray, closest_t);
 	P = vector_add(camera(), &D);
 	N = vector_sub(&P, &closest_sphere->center);
-	N = vector_normalize(&N);
-	//return (closest_sphere->color);
 	return (color_mult(&closest_sphere->color, calculate_light(&P, &N)));
 }
 
@@ -107,72 +109,7 @@ t_Point	intersects_sphere(t_Vector D, t_Sphere sphere)
 		intersections.t1 = INT_MAX;
 		intersections.t2 = INT_MAX;
 	}
-	intersections.t1 = (-b + sqrt(discriminant) / (2 * a));
-	intersections.t2 = (-b - sqrt(discriminant) / (2 * a));
+	intersections.t1 = (-b + sqrt(discriminant)) / (2 * a);
+	intersections.t2 = (-b - sqrt(discriminant)) / (2 * a);
 	return (intersections);
 }
-
-/*
-	Equação do raio/ray:
-	 _______________________________________________________
-	|														|
-	|	P = O + tR											|
-	|_______________________________________________________|
-
-	O = origem do raio ( camera )
-	t = distância
-	R = direcção ( ponto final - ponto inicial, normalizado)
-
-	Equaçao da esfera:
-	 _______________________________________________________   
-	|														|
-	|	(a - x)² + (b - y)² + (c - z)² - r² = 0				|
-	|_______________________________________________________|
-
-	- a e b e c representam algum ponto no nosso plano cartesiano.
-	- x e y e z representam o centro da nossa esfera.
-*/
-
-/*
-	Equação do raio/ray:
-	 ________________________________________________________________
-	|																|
-	|	P = O + tR													|
-	|_______________________________________________________________|
-
-	O = origem do raio ( vetor 2D, indica posição inicial)
-	t = distância
-	R = direcção (vetor unitário )
-
-	Equaçao da circulo:
-	 _______________________________________________________________
-	|																|
-	|	(a - x)² + (b - y)² - r² = 0								|
-	|_______________________________________________________________|
-	
-	Equaçao da esfera:
-	 _______________________________________________________________
-	|																|
-	|	(a - x)² + (b - y)² + (c - z)² - r² = 0						|
-	|_______________________________________________________________|
-
-	- a e b representam algum ponto no nosso plano cartesiano.
-	- x e y representam o centro do nosso círculo.
-
-int	intersects_circle(int x, int y)
-{
-	float	sph_x_squared;
-	float	sph_y_squared;
-	float	sph_z_squared;
-	float	radius_squared;
-
-	sph_x_squared = pow(x - sphere()->center.x, 2);
-	sph_y_squared = pow(y - sphere()->center.y, 2);
-	sph_z_squared = pow(y - sphere()->center.z, 2);
-	radius_squared = pow(sphere()->radius, 2);
-
-	if (sph_x_squared + sph_y_squared + sph_z_squared - radius_squared <= 0)
-		return (1);
-	else
-		return (0);
-}*/
