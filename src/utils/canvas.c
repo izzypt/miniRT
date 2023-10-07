71fd51c4006f74b0e5ec99972e83eb6aaef74e34
Simao <bonvalots@gmail.com>
2023-10-07 18:57:40 +0100

    angleBetween vectors added

--- src/utils/canvas.c
+++ src/utils/canvas.c
@@ -6,7 +6,7 @@
 /*   By: simao <simao@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 22:46:50 by simao             #+#    #+#             */
-/*   Updated: 2023/10/02 13:01:53 by simao            ###   ########.fr       */
+/*   Updated: 2023/10/07 18:57:06 by simao            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,24 @@ void	put_pixel(int x, int y, t_Color color)
 	mlx_pixel_put(mlibx()->mlx, mlibx()->win, new_x, new_y, hex_color);
 }
 
+float	AngleBetweenVectors(t_Vector vector1, t_Vector vector2)
+{
+	float	magnitude1;
+	float	magnitude2;
+	float	dot;
+	float	radians;
+	float	degrees;
+
+	magnitude1 = vector_magnitude(vector1);
+	magnitude2 = vector_magnitude(vector2);
+	dot = dot_product(vector1, vector2);
+	radians = (acos(dot / (magnitude1 * magnitude2)));
+
+	degrees = radians * (180.0 / PI);
+
+	return (degrees);
+}
+
 /*
  - Converts canvas coordinates into viewport coordinates.
  - Stores them in raydir.
@@ -38,9 +56,14 @@ void	put_pixel(int x, int y, t_Color color)
 t_Vector	canvas_to_viewport(int x, int y)
 {
 	t_Vector	ray;
+	t_Vector	orientation;
 
+	orientation.x = 0;
+	orientation.y = 0;
+	orientation.z = 1;
 	ray.x = x * ((float)viewport()->width / (float)canvas()->width);
 	ray.y = y * ((float)viewport()->height / (float)canvas()->height);
 	ray.z = viewport()->dist;
+	printf("Angle between ray and orientation vector: %f\n", AngleBetweenVectors(ray, orientation));
 	return (ray);
 }
