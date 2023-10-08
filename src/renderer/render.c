4556f7e295633f5adc44596ea7d29e7f82070617
Simao <bonvalots@gmail.com>
2023-10-08 17:23:30 +0100

    rotation camera added to the renderer

--- src/renderer/render.c
+++ src/renderer/render.c
@@ -6,7 +6,7 @@
 /*   By: simao <simao@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 22:33:12 by simao             #+#    #+#             */
-/*   Updated: 2023/10/03 17:16:48 by simao            ###   ########.fr       */
+/*   Updated: 2023/10/08 16:54:25 by simao            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,16 @@ void	render(void)
 	int			x;
 	int			y;
 	t_Vector	d;
+	t_RotMatrix	rotation_matrix;
 
+	rotation_matrix = rotate_camera();
 	x = ((canvas()->width / 2) * -1);
 	y = (canvas()->height / 2);
 	while (--y >= (canvas()->height / 2) * -1)
 	{
 		while (++x <= (canvas()->width / 2))
 		{
-			d = canvas_to_viewport(x, y);
+			d = mult_mtrx_vector(&rotation_matrix, canvas_to_viewport(x, y));
 			color = trace_ray(d, 1, INT_MAX);
 			put_pixel(x, y, color);
 		}
